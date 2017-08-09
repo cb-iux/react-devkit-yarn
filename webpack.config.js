93a1694aed836424d31e945ad3a3dc688453d8c0
chris-work <cb@iux.no>
2017-08-09 19:05:15 +0200

    setup complete

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader'},
+      {test: /\.scss$/, use: ['style-loader','css-loader','sass-loader']}
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    stats: "errors-only"
+  },
+  plugins: [new HtmlWebpackPlugin ({
+    minify: {
+      collapseWhitespace: true
+    },
+    template: 'src/index.html'
+  })]
+}
